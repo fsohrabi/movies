2f7f9625dea0d612b44ac19b3e4ef0936e6c205c
fsohrabi <fs.sohrabi70@gmail.com>
2024-09-09 23:23:15 +0200

    add  Command Line Arguments

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from app.movie_app import MovieApp
 from storage.storage_json import StorageJson
+import argparse
 
 
 def main():
     """Main function to display the menu and handle user input."""
-    storage = StorageJson('movies.json')
+    parser = argparse.ArgumentParser(description='Get user filename to save movies')
+    parser.add_argument('-n', '--name', type=str, required=True, help='name of file')
+    args = parser.parse_args()
+    storage = StorageJson(args.name)
     movie_app = MovieApp(storage)
     movie_app.run()
 
